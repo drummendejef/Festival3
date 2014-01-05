a2e81981658e43a97adb75d15d951e0b9269eb3b
Joren <jorenvandamme@hotmail.com>
2014-01-05 16:20:05 +0100

    Validatie werkt!
    Tabblad contactpersoon en de festivalnaam.

---
+++ README.txt
@@ -0,0 +1,2 @@
+Dit is een schoolopdracht voor Bussiness Programming.
+Doel is een app te maken die een festival kan handhaven.
--- model/ContactPerson.cs
+++ model/ContactPerson.cs
@@ -1,23 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Data.Common;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 
 namespace Festival.model
 {
-    class ContactPerson
+    class ContactPerson : INotifyPropertyChanged, IDataErrorInfo
     {
         public int ID { get; set; }
-        [Required(ErrorMessage = "De naam is verplicht")]
+        [Required(ErrorMessage = "De voornaam is verplicht")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "De naam moet tussen de 3 en 50 karakters bevatten ")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "De voornaam is verplicht")]
+        [Required(ErrorMessage = "De naam is verplicht")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "De voornaam moet tussen de 3 en 50 karakters bevatten ")]
         public string Surname { get; set; }
         [Required(ErrorMessage = "Het Bedrijf is verplicht")]
@@ -31,9 +33,16 @@ namespace Festival.model
         [Required(ErrorMessage = "De straat is verplicht")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "De straatnaam moet tussen de 3 en 50 karakters bevatten ")]
         public string Street { get; set; }
+        [Required(ErrorMessage = "Postcode is verplicht")]
+        [StringLength(4, MinimumLength = 4, ErrorMessage = "Postcode moet 4 tekens bevatten")]
         public int Zipcode { get; set; }
+        [Required(ErrorMessage = "Emailadres is verplicht")]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Telefoonnummer is verplicht")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[/. ]?([0-9]{2})[.. ]?([0-9]{2})[..]?([0-9]{2})$", ErrorMessage = "Telefoonnummer niet correct.")]
         public string Phone { get; set; }
+        [RegularExpression(@"^\(?([0-9]{4})\)?[/. ]?([0-9]{2})[.. ]?([0-9]{2})[..]?([0-9]{2})$", ErrorMessage = "GSMnummer niet correct.")]
         public string Cellphone { get; set; }
 
 
@@ -51,6 +60,11 @@ namespace Festival.model
                 ContactPerson contact = Create(record);
                 contacts.Add(contact);
             }
+
+            //Database verbinding sluiten.
+            if (reader != null)
+                reader.Close();
+
             return contacts;
         }
 
@@ -88,7 +102,7 @@ namespace Festival.model
         {
             int jobroleid = ContactPersonType.GetContactTypeID(p.JobRole);
 
-            string sql = "UPDATE Werknemers SET Voornaam=@Name, Naam=@Surname, Straat_Nr=@Street, Gemeente=@City, Postcode=@Zipcode, GSM=@Cellphone, JobID=@Jobrole, Bedrijf=@Company WHERE ID=@ID";
+            string sql = "UPDATE Werknemers SET Voornaam=@Name, Naam=@Surname, Straat_Nr=@Street, Gemeente=@City, Postcode=@Zipcode, GSM=@Cellphone, JobID=@Jobrole, Bedrijf=@Company, Telefoon=@Phone, Email= WHERE ID=@ID";
 
 
             DbParameter par1 = Database.AddParameter("@Name", p.Name);
@@ -104,9 +118,7 @@ namespace Festival.model
             DbParameter par11 = Database.AddParameter("@Company", p.Company);
             Database.ModifyData(sql, par1, par2, par3, par4, par5, par6, par7, par8, par9, par10, par11);
 
-            //trans.Commit();
-
-            //Database.ReleaseConnection(trans.Connection);
+            Console.WriteLine("Persoon geupdate");
         }
 
         //Nieuwe contactpersoon toevoegen
@@ -151,6 +163,19 @@ namespace Festival.model
             }
         }
 
+        /* ----------------------------------------------------------------- */
+        // PROPERTY CHANGED EVENTHANDLER
+        /* ----------------------------------------------------------------- */
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
         //IDataErrorinfo interface geimplementeerd.
         public string Error
         {
@@ -174,5 +199,6 @@ namespace Festival.model
             }
         }
         //Einde data validation
+        
     }
 }
